a5ad4aefb39b445b6f879764a21ffc3d725a82a9
gabriel <156666327+gabrielghr@users.noreply.github.com>
2024-10-18 10:12:18 +0200

    feature added counter variable

--- exercise-001/main.cpp
+++ exercise-001/main.cpp
@@ -10,11 +10,17 @@ auto main(int argc, char **argv) -> int
      * CLI11 is a command line parser to add command line options
      * More info at https://github.com/CLIUtils/CLI11#usage
      */
-    CLI::App app{PROJECT_NAME};
+    auto counter = 20;      // set a default of 20
+    CLI::App app{PROJECT_NAME};     // create an instance of an App with variable name app
     try
     {
         app.set_version_flag("-V,--version", fmt::format("{} {}", PROJECT_VER, PROJECT_BUILD_DATE));
+        // Add a command line option -c or --count and fill the variable counter
+        app.add_option("-c,--count", counter, "How many iteams to insert");
         app.parse(argc, argv);
+
+
+
     }
     catch (const CLI::ParseError &e)
     {
@@ -29,6 +35,11 @@ auto main(int argc, char **argv) -> int
     fmt::print("Hello, {}!\n", app.get_name());
 
     /* INSERT YOUR CODE HERE */
+    fmt::print("Value of argc, {}!\n", argc);
+    fmt::print("Value of argv[0], {}!\n", argv[0]);
+    //fmt::print("Value of argv[2], {}!\n", argv[2]); //possible nullpointer exception
 
-    return 0; /* exit gracefully*/
+    fmt::print("The value of the counter, {}!\n", counter);
+
+    return 42; /* exit gracefully*/
 }
